e999e091bf6245c3336c11a3bc7b142a87fc1356
alfcadmorUS <alfcadmor@alum.us.es>
2022-03-14 19:50:55 +0100

    Task: 038 and 039 undone

--- src/main/java/acme/entities/announcement/Annoucenment.java
+++ src/main/java/acme/entities/announcement/Annoucenment.java
@@ -1,15 +1,6 @@
 package acme.entities.announcement;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
-
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -25,26 +16,6 @@ public class Annoucenment extends AbstractEntity{
 	protected static final long serialVersionUID= 1L;
 	
 	
-	//Attributes
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Past
-	protected Date creationMoment;
-	
-	@NotBlank
-	@Length(min=1,max=101)
-	protected String title;
-	
-	@NotBlank
-	@Length(min=1,max=256)
-	protected String body;
-	
-	
-	protected Boolean flag;
-	
-	
-	@URL
-	protected String link;
 	
 	
 }
