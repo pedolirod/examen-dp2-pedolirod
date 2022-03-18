8f82283291af7f009eff1437673fd2673e0f6c30
alefrarod <alefrarod@alum.us.es>
2022-03-18 01:06:34 +0100

    Task-042: Create ToolKit done

--- src/main/java/acme/artifact/PartOf.java
+++ src/main/java/acme/artifact/PartOf.java
@@ -2,12 +2,17 @@ package acme.artifact;
 
 import java.awt.Toolkit;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import acme.framework.entities.AbstractEntity;
-
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
 public class PartOf extends AbstractEntity{
 	
 	// Serialisation identifier -----------------------------------------------
