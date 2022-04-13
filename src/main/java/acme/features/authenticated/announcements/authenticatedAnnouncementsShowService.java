8b37726d534dd5e915dbbb6d926892bbd65ac3ff
antquiher1 <antquiher1@alum.us.es>
2022-04-13 19:35:54 +0200

    Create announcements views for authenticated principals

---
+++ src/main/java/acme/features/authenticated/announcements/authenticatedAnnouncementsShowService.java
@@ -0,0 +1,60 @@
+package acme.features.authenticated.announcements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.announcement.Announcement;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Any;
+import acme.framework.roles.Authenticated;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class authenticatedAnnouncementsShowService implements AbstractShowService<Authenticated, Announcement> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected authenticatedAnnouncementsRepository repository;
+
+	// AbstractShowService<Anonymous, Announcement> interface --------------------------
+
+	@Override
+	public boolean authorise(final Request<Announcement> request) {
+		assert request != null;
+
+		boolean result;
+		int id;
+		Announcement Announcement;
+
+		id = request.getModel().getInteger("id");
+		Announcement = this.repository.findOneAnnouncementById(id);
+		result = Announcement.isFlag();
+
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Announcement> request, final Announcement entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "title", "creationMoment", "body", "flag", "link");
+	}
+
+	@Override
+	public Announcement findOne(final Request<Announcement> request) {
+		assert request != null;
+
+		Announcement result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneAnnouncementById(id);
+
+		return result;
+	}
+
+}
--- target/classes/WEB-INF/views/fragments/menu.jsp
+++ target/classes/WEB-INF/views/fragments/menu.jsp
@@ -49,6 +49,7 @@
 	<acme:menu-right>
 		<acme:menu-option code="master.menu.sign-up" action="/anonymous/user-account/create" access="isAnonymous()"/>
 		<acme:menu-option code="master.menu.sign-in" action="/master/sign-in" access="isAnonymous()"/>
+		<acme:menu-option code="master.menu.announcement" action="/authenticated/announcement/list" access="isAuthenticated()"/>
 
 		<acme:menu-option code="master.menu.user-account" access="isAuthenticated()">
 			<acme:menu-suboption code="master.menu.user-account.general-data" action="/authenticated/user-account/update"/>
