19d27fc803b9ade5027a34582a81f5cff78c4b4d
pedolirod <100024060+pedolirod@users.noreply.github.com>
2022-04-25 19:46:27 +0200

    Task 76: Completed

--- src/main/java/acme/features/administrator/AdminDashboard/AdminDashboardRepository.java
+++ src/main/java/acme/features/administrator/AdminDashboard/AdminDashboardRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import acme.artifact.Artifact;
 import acme.artifact.ArtifactType;
+import acme.datatypes.StatusType;
+import acme.entities.patronage.Patronage;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -17,4 +19,7 @@ public interface AdminDashboardRepository extends AbstractRepository {
 
 	@Query("select a from Artifact a where  a.technology=:technology and a.type = :artifact_type")
 	Collection<Artifact> findArtifactTechnologyCurrency(String technology, ArtifactType artifact_type);
+
+	@Query("select p from Patronage p where  p.status=:status")
+	Collection<Patronage> findPatronage(StatusType status);
 }
