c6519ad31933733f4ab62d8f24c3502355378f3f
antquiher1 <antquiher1@alum.us.es>
2022-04-25 19:46:22 +0200

    puesta en comun de task 69

--- src/main/java/acme/features/any/partOf/anyPartOfListService.java
+++ src/main/java/acme/features/any/partOf/anyPartOfListService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.artifact.PartOf;
-import acme.artifact.ToolKit;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
 import acme.framework.roles.Any;
@@ -24,13 +23,7 @@ public class anyPartOfListService implements AbstractListService<Any,PartOf> {
 	public boolean authorise(final Request<PartOf> request) {
 		assert request != null;
 		
-		final int masterId = request.getModel().getInteger("masterId");
-		
-		final ToolKit toolKit = this.repository.findOneToolKitById(masterId);
-		
-		//result = !toolKit.isDraftMode();
-		
-		return true; //result 
+		return true;
 	}
 
 	@Override
